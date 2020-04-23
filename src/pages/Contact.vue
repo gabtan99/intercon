c669180c00cd1f01e498e3a784dd72ff05d95f6f
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-04-23 18:36:23 +0800

    fix: polished contact us frontend

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -5,32 +5,34 @@
           <b-col sm="5">
             <div class="form-container">
               <div class="contact-header">
-                <h1 class="font-gilroy-medium font-36">Get in touch</h1>
-                <p class="font-avenir-light font-18">We're here to help and answer any questions you might have. Looking forward to hearing from you!</p>
+                <h1 class="font-gilroy-medium font-36 blue">Get in touch</h1>
+                <p class="font-avenir-light font-18 blue">We're here to help and answer any questions you might have. Looking forward to hearing from you!</p>
               </div>
 
               <form class="contact-form" name="contact">
 
                 <div class="sender-info">
                   <div class="form-item">
-                    <label for="name" class="font-gilroy-regular font-16 required">Name</label>
-                    <input type="text" name="name"/>
+                    <label for="name" class="font-gilroy-regular font-16 blue required">Name</label>
+                    <input type="text" class="font-gilroy-regular font-16 required" name="name" autocomplete="off"/>
                   </div>
                   <div class="form-item">
-                    <label for="email" class="font-gilroy-regular font-16 required">Email Address</label>
-                    <input type="email" name="email"/>
+                    <label for="email" class="font-gilroy-regular font-16 blue required">Email Address</label>
+                    <input type="email" class="font-gilroy-regular font-16 required" name="email" autocomplete="off"/>
                   </div>
                   <div class="form-item">
-                    <label for="subject" class="font-gilroy-regular font-16 required">Subject</label>
-                    <select id="subject" name="subject" class="font-gilroy-regular font-16">
-                      <option value="1">Apple</option>
-                      <option value="2">Banana</option>
-                      <option value="3">Pineapple</option>
+                    <label for="subject" class="font-gilroy-regular font-16 blue required">Subject</label>
+                    <select id="subject" name="subject" class="options font-gilroy-regular font-16 blue">
+                      <option disabled selected value style="display:none"> -- select a subject -- </option>
+                      <option value="prices">Prices</option>
+                      <option value="services">Services</option>
+                      <option value="targets">Targets</option>
+                      <option value="others">Others</option>
                     </select>
                   </div>
                   <div class="form-item">
-                    <label for="message" class="font-gilroy-regular font-16 required">Message</label>
-                    <textarea name="message"></textarea>
+                    <label for="message" class="font-gilroy-regular font-16 blue required">Message</label>
+                    <textarea name="message" class="font-gilroy-regular font-16 blue required"></textarea>
                   </div>
                 </div>
           
@@ -51,20 +53,20 @@
                   <b-row> 
 
                     <div class="col-sm-6 visit-us">
-                      <h2 class="font-gilroy-medium font-18">Visit Us</h2>
+                      <h2 class="font-gilroy-medium font-18 blue">Visit Us</h2>
                       <span class="contact-info">
-                        <p class="font-avenir-light font-16">
+                        <p class="font-avenir-light font-16 blue">
                           210B Del Monte Ave, La Loma, Quezon City, 1114 Metro Manila
                         </p>
                         <br>
-                        <p class="font-avenir-light font-16">
+                        <p class="font-avenir-light font-16 blue">
                           8AM - 8PM
                         </p>
                       </span>
                     </div>
                     <div class="col-sm-1"/>
                     <div class="col-sm-5">
-                      <h2 class="font-gilroy-medium font-18">Get Social</h2>
+                      <h2 class="font-gilroy-medium font-18 blue">Get Social</h2>
                       <span class="contact-info">
                         <a href="https://www.facebook.com/interconregenerative/">
                           <font-awesome
@@ -106,6 +108,13 @@ export default {}
 
 .form-item {
   width: 100%;
+}
+
+select {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  padding: 5px;
 }
 
 .card {
@@ -157,12 +166,12 @@ label.required::after {
 }
 
 input:focus,textarea:focus {
-  border-color: var(--color-contrast-1);
+  border-color: #006838;
 }
 
 input,textarea,select {
   background: transparent;
-  border: 1px solid var(--color-base-1);
+  border: 1px solid var(--gray-2);
   outline: none;
   border-radius: 0.3rem;
   padding: 0.8rem 1rem;
@@ -177,7 +186,7 @@ textarea {
 }
 
 .button {
-  color: var(--color-base);
+  color: var(--white);
   background: #006838;
   outline: none;
   border: 0;
@@ -206,11 +215,15 @@ textarea {
 .social {
   margin: 2px;
   margin-right: 10px;
-  color: var(--black);
+  color: var(--gray-5);
   font-size: 24px;
 }
 
 .social:hover {
   color: var(--gray-3);
+}
+
+.blue {
+  color: #123450
 }
 </style>
