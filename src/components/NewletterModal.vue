e928b388b4fbcfdc2e5fdd40c7a6f5408c83f759
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-04-25 00:27:16 +0800

    feat: modal

---
+++ src/components/NewletterModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-modal id="newsletter" hide-footer="true" hide-header="true">
+    <img src="../assets/img/icon-exit.png" class="exit"/>
+    <p class="my-4">Hello from modal!</p>
+  </b-modal>
+</template>
+
+<style scoped>
+  .exit{
+    position: absolute;
+    top: 30px;
+    right: 36px;
+  }
+</style>
