55cd3b9904350832f67519624637905ed3f9231b
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-04-27 21:26:05 +0800

    feat (wip): about us page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,98 +1,87 @@
 <template>
   <Layout>
-    <div class="container">
+    <PageHeader :page_name="page_name" :image="image" />
 
-      <div class="contact-header">
-        <h1 class="contact-title">About</h1>
-        <p>Leave me a note with any questions you might have, I'll get back to you as soon as possible.</p>
-      </div>
-
-      <form class="contact-form" name="contact">
-
-        <div class="sender-info">
-          <div>
-            <label for="name" class="label">Your name</label>
-            <input type="text" name="name" />
-          </div>
-          <div>
-            <label for="email" class="label">Your email</label>
-            <input type="email" name="email" />
-          </div>
+    <div class="main">
+      
+    
+      <b-row class="founder-section">
+        <div class="col-lg-6">
+          
         </div>
 
-        <div class="message">
-          <label for="message" class="label">Message</label>
-          <textarea name="message"></textarea>
+        <div class="col-lg-6">
+          <h1 class="font-gilroy-bold font-36">
+            The Founder
+          </h1>
+
+          <h1 class="font-avenir-light font-21">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          </h1>
         </div>
+      </b-row>
 
-        <button class="button">Submit form</button>
+      <b-row class="mission-vision-section">
+        <div class="col-lg-6">
+          <h1 class="font-gilroy-bold font-36">
+            Our Goal
+          </h1>
 
-      </form>
+          <h1 class="font-avenir-light font-21">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          </h1>
+        </div>
+        
+        <div class="col-lg-6">
+          <h1 class="font-gilroy-bold font-36">
+            Our Mission
+          </h1>
 
+          <h1 class="font-avenir-light font-21">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          </h1>
+        </div>
+      </b-row>
+
+      <b-row class="history-section">
+
+      </b-row>
     </div>
   </Layout>
 </template>
 
 <script>
-export default {}
+import PageHeader from '@/components/PageHeader'
+import NewsletterModal from "@/components/NewsletterModal"
+
+export default {
+  data(){
+    return {
+      page_name: 'About Us',
+      image: 'about-header.png',
+    }
+  },
+  components: {
+    PageHeader,
+    NewsletterModal,
+  },
+}
 </script>
 
 <style scoped>
-.contact-header {
-  padding: 2rem 0 4rem 0;
-}
-.contact-title {
-  font-size: 4rem;
-  margin: 0 0 4rem 0;
-  padding: 0;
-}
-.sender-info {
+.main {
   display: flex;
+  flex-direction: row;
   flex-wrap: wrap;
-  margin-bottom: 2rem;
+  padding: 80px 15px;
+  justify-content: center;
+  padding-bottom: 40px;
 }
-.sender-info > div {
-  flex: 1;
-  margin-right: 4rem;
-}
-.sender-info > div:last-of-type {
-  margin: 0;
-}
-input:focus,textarea:focus {
-  border-color: var(--color-contrast-1);
-}
-input,textarea {
-  background: transparent;
-  border: 1px solid var(--color-base-1);
-  outline: none;
-  border-radius: 0.3rem;
-  padding: 0.8rem 1rem;
-  color: inherit;
-  font-size: 1rem;
-  width: 100%;
-}
-textarea {
-  resize: none;
-  height: 140px;
-}
-.button {
-  color: var(--color-base);
-  background: var(--color-contrast);
-  outline: none;
-  border: 0;
-  font-size: 0.8rem;
-  padding: 0.8rem 1.6rem;
-  border-radius: 0.3rem;
-  margin-top: 2rem;
-  cursor: pointer;
-  transition: opacity 0.25s ease;
-  font-size: 500;
-  letter-spacing: 0.035em;
-}
-.button:hover {
-  opacity: 0.6;
-}
-.button:focus {
-  border: 1px solid var(--color-base-1);
+
+.mission-vision-section {
+  width: 90%;
 }
 </style>
