eedeabd33471b52da7729fe51ab0f57ae45a6429
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-04-28 23:05:15 +0800

    test: removed jquery but scroll reveal remains

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -51,41 +51,38 @@
 
 
 <script>
-import JQuery from 'jquery'
 import ScrollReveal from 'scrollreveal'
-
-let $ = JQuery
 
 export default {
     mounted() {
         let sr = ScrollReveal({ reset: true });
 
-        if ($(window).width() < 768) {
-            if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-                $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-            }
+        // if ($(window).width() < 768) {
+        //     if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+        //         $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+        //     }
 
-            sr.reveal('.js--fadeInRight', {
-                origin: 'right',
-                distance: '300px',
-                easing: 'ease-in-out',
-                duration: 800,
-                });
-        } else {
-            sr.reveal('.js--fadeInLeft', {
-            origin: 'left',
-            distance: '300px',
-            easing: 'ease-in-out',
-            duration: 800,
-            });
+        //     sr.reveal('.js--fadeInRight', {
+        //         origin: 'right',
+        //         distance: '300px',
+        //         easing: 'ease-in-out',
+        //         duration: 800,
+        //         });
+        // } else {
+        //     sr.reveal('.js--fadeInLeft', {
+        //     origin: 'left',
+        //     distance: '300px',
+        //     easing: 'ease-in-out',
+        //     duration: 800,
+        //     });
 
-            sr.reveal('.js--fadeInRight', {
-            origin: 'right',
-            distance: '300px',
-            easing: 'ease-in-out',
-            duration: 800,
-            });
-        }
+        //     sr.reveal('.js--fadeInRight', {
+        //     origin: 'right',
+        //     distance: '300px',
+        //     easing: 'ease-in-out',
+        //     duration: 800,
+        //     });
+        // }
 
         sr.reveal('.js--fadeInLeft', {
         origin: 'left',
