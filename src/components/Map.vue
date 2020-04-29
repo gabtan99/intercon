d5b2d9efb2e7d7e2ff91d58e28edbeee35942ddf
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-04-29 15:42:39 +0800

    feat: fixed openlayers

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,10 @@
 <template>
-  <vl-map
+<div>
+
+  <div id="testmap"></div>
+  <img id="pointer" src="@/assets/img/map-marker.png" height="50px" />
+</div>
+  <!-- <vl-map
     :load-tiles-while-animating="true"
     :load-tiles-while-interacting="true"
     data-projection="EPSG:4326"
@@ -13,22 +18,59 @@
 
     <vl-overlay id="overlay" :position="overlayCoordinate" :positioning="overlayPositioning">
       <template>
-        <img src="@/assets/img/map-marker.png" height="50px"/>
+        <img src="@/assets/img/map-marker.png" height="50px" />
       </template>
     </vl-overlay>
-  </vl-map>
+  </vl-map>-->
 </template>
 
 <script>
+import "ol/ol.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import { transform } from "ol/proj";
+import Overlay from "ol/Overlay";
+
+function initMap() {
+  let map = new Map({
+    target: "testmap",
+    layers: [new TileLayer({ source: new OSM() })],
+    view: new View({
+      projection: "EPSG:4326",
+      center: [120.9954475, 14.637538],
+      zoom: 19
+    })
+  });
+
+  var popup = new Overlay({
+    element: document.getElementById('pointer'),
+    positioning: "bottom-center"
+  });
+  popup.setPosition([120.995388, 14.63757]);
+  map.addOverlay(popup);
+}
+
 export default {
-  data() {
-    return {
-      zoom: 19,
-      center:  [120.9954475, 14.637538],
-      rotation: 0,
-      overlayCoordinate: [120.995388, 14.63757],
-      overlayPositioning: "bottom-center"
-    };
+  // data() {
+  // return {
+  //   zoom: 19,
+  //   center: [120.9954475, 14.637538],
+  //   rotation: 0,
+  //   overlayCoordinate: [120.995388, 14.63757],
+  //   overlayPositioning: "bottom-center"
+  // };
+  // },
+  mounted() {
+    this.$nextTick(function() {
+      initMap();
+    });
   }
 };
 </script>
+
+<style scoped>
+#testmap {
+  height: 300px;
+}
+</style>
