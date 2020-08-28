f273607d9ccbe0690046eff4d2e82d506f77e2f4
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-08-28 15:13:58 +0800

    feat: unit test for Hero section component

--- tests/unit/Hero.spec.js
+++ tests/unit/Hero.spec.js
@@ -1,19 +1,41 @@
-import { mount } from '@vue/test-utils'
-import sinon from 'sinon'
+import { mount, createLocalVue } from '@vue/test-utils'
+import BootstrapVue from 'bootstrap-vue'
 
 import Hero from '@/components/Hero'
-
 import GreenButton from "@/components/GreenButton";
 import WhiteButton from "@/components/WhiteButton";
 import YoutubeModal from "@/components/YoutubeModal";
 
-describe('Hero Section Component', () => {
-  it("renders GreenButton, WhiteButton, and YoutubeModal", () => {
-    const wrapper = mount(Hero)
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
 
+describe('Hero Section Component', () => {
+
+  let watchVideo = jest.fn()
+  let learnMore = jest.fn()
+  const wrapper = mount(Hero, {
+    methods: {
+      watchVideo,
+      learnMore,
+    },
+    localVue
+  })
+
+  it("renders components GreenButton, WhiteButton, and YoutubeModal", () => {
     expect(wrapper.findComponent(GreenButton).exists()).toBe(true)
     expect(wrapper.findComponent(WhiteButton).exists()).toBe(true)
     expect(wrapper.findComponent(YoutubeModal).exists()).toBe(true)
   })
-  //add a test that checks if green button is clicked, the youtube modal shows up
+
+  it('calls watchVideo method when Watch button is clicked', () => {
+    expect(watchVideo).not.toHaveBeenCalled()
+    wrapper.findComponent(WhiteButton).trigger('click')
+    expect(watchVideo).toHaveBeenCalled()
+  })
+
+  it('calls learnMore method when Learn More button is clicked', () => {
+    expect(learnMore).not.toHaveBeenCalled()
+    wrapper.findComponent(GreenButton).trigger('click')
+    expect(learnMore).toHaveBeenCalled()
+  })
 })
