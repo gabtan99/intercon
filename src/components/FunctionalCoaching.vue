c0c47b1f619bd470e601892821d1e9c5c28aa714
Gab Tan <gabtan99@gmail.com>
2020-08-29 12:23:26 +0800

    refactor: services // newlestter

---
+++ src/components/FunctionalCoaching.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-row no-gutters class="mx-4 mb-5 px-3 py-4 card">
+    <div class="col-lg-7">
+      <h2>Functional Health and Wellness Coaching</h2>
+      <p>
+        Hyperbaric oxygen therapy involves breathing pure oxygen in a
+        pressurized room or tube. Hyperbaric oxygen therapy is a
+        well-established treatment for decompression sickness, a hazard of scuba
+        diving.
+      </p>
+    </div>
+    <div class="col-lg-3">Hey</div>
+  </b-row>
+</template>
+
+<style scoped>
+.card {
+  border: 0px;
+  box-shadow: 0px 3px 6px var(--gray-2);
+  -webkit-box-shadow: 0px 3px 6px var(--gray-2);
+  -moz-box-shadow: 0px 3px 6px var(--gray-2);
+  -o-box-shadow: 0px 3px 6px var(--gray-2);
+  -ms-box-shadow: 0px 3px 6px var(--gray-2);
+}
+</style>
