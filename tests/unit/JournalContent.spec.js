395dd07c2d3eef93132179d2bb5b6fdedb2523e6
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-08-30 22:23:26 +0800

    feat: tentative unit test for JournalContent component

---
+++ tests/unit/JournalContent.spec.js
@@ -0,0 +1,23 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import BootstrapVue from 'bootstrap-vue'
+
+import JournalContent from '@/components/JournalContent'
+
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+
+describe('Journal Content Component', () => {
+
+    const wrapper = mount(JournalContent, {
+        propsData: {
+            title: "Journal Content Title goes here",
+            content: "Journal Content actual Content goes here",
+        },
+        localVue
+    })
+
+    it("renders components GreenButton, WhiteButton, and YoutubeModal", () => {
+        expect(wrapper.props().title).toBe('Journal Content Title goes here');
+        expect(wrapper.props().content).toBe('Journal Content actual Content goes here');
+    })
+})
