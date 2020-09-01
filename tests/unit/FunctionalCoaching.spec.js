8f28ade94d252a8de7dbcbb668a885ba7d54b915
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-09-01 11:24:26 +0800

    fix: adjusted unit test for FunctionalCoaching component

--- tests/unit/FunctionalCoaching.spec.js
+++ tests/unit/FunctionalCoaching.spec.js
@@ -12,12 +12,26 @@ describe('Functional Coaching Component', () => {
     let goToService = jest.fn()
     const wrapper = mount(FunctionalCoaching, {
         propsData: {
-            service: {},
+            coaching: {
+                title: "Functional Health and Wellness Coaching",
+                short_description: "Functions of Health and Coaching with Wellness",
+                facebook_page_url: "Sage",
+                email: "My power does not end. Ask for aid and you shall receive it. Stand tall. We are VALORANT, we are fighters!",
+                thumbnail_image: "https://res.cloudinary.com/dgq7s4lkp/image/upload/v1598761543/uploads_dev/placeholder_fww9hi.png"
+            },
         },
         methods: {
             goToService,
         },
         localVue
+    })
+
+    it("passes all props of FunctionalCoaching component properly", () => {
+        expect(wrapper.props().coaching.title).toBe('Functional Health and Wellness Coaching');
+        expect(wrapper.props().coaching.short_description).toBe('Functions of Health and Coaching with Wellness');
+        expect(wrapper.props().coaching.facebook_page_url).toBe('Sage');
+        expect(wrapper.props().coaching.email).toBe('My power does not end. Ask for aid and you shall receive it. Stand tall. We are VALORANT, we are fighters!');
+        expect(wrapper.props().coaching.thumbnail_image).toBe('https://res.cloudinary.com/dgq7s4lkp/image/upload/v1598761543/uploads_dev/placeholder_fww9hi.png');
     })
 
     it('calls goToService method when entire block is clicked', () => {
