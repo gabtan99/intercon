7cd09025030cef13dde1a6c4467e54ded031b6c8
Gabriel Tan <gabtan99@gmail.com>
2020-09-25 15:32:27 +0800

    Feat: coming soon fallback

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    <h1 class="font-gilroy-bold font-36">Coming soon...</h1>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  margin: 50px auto;
+  margin-bottom: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--gray-2);
+}
+</style>
