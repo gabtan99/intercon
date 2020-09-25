e58ab2d37b77204b20a193d3365aab66298a10f6
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-09-25 19:24:02 +0800

    feat: integrated contact details forestry in footer component

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,9 +3,23 @@
     <Header />
     <slot />
     <NewsletterModal />
-    <Footer />
+    <Footer 
+      :address="$static.contactDetails.address"
+      :phone_number="$static.contactDetails.phone_number"
+      :mobile_number="$static.contactDetails.mobile_number"
+    />
   </div>
 </template>
+
+<static-query>
+query Posts {
+  contactDetails: pages (path: "/data/contact-details/"){
+    address
+    phone_number
+    mobile_number
+  },
+}
+</static-query>
 
 <script>
 import Header from '@/components/Header'
