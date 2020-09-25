82d7d48c7246a2fb9a8beac4ad65e3dc9361bcaa
Gabriel Tan <gabtan99@gmail.com>
2020-09-25 10:41:07 +0800

    test: social medai share btutons

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -4,14 +4,14 @@
       <div class="journal-header">
         <img class="header-images" :src="$page.post.header_image" />
         <h4 class="date-style font-gilroy-light font-16">
-          {{ $page.post.date | moment('LL') }}
+          {{ $page.post.date | moment("LL") }}
         </h4>
         <h2 v-html="$page.post.title" class="journal-title font-gilroy-bold" />
         <div class="author-div font-gilroy-regular font-16">
           by
-
           {{ $page.post.author }}
         </div>
+        <SocialShareButtons />
       </div>
       <JournalContent :content="$page.post.content" :title="$page.post.title" />
     </div>
@@ -33,55 +33,59 @@ query BlogPost ($path: String!) {
 </page-query>
 
 <script>
-import JournalContent from '@/components/JournalContent'
-import moment from 'vue-moment'
+import JournalContent from "@/components/JournalContent";
+import SocialShareButtons from "@/components/SocialShareButtons";
+import moment from "vue-moment";
 export default {
   metaInfo() {
     return {
-      title: `${this.$page.post.title} | Services | Intercon Regenerative Center`,
+      title: `${
+        this.$page.post.title
+      } | Services | Intercon Regenerative Center`,
       meta: [
         {
-          key: 'description',
-          name: 'description',
+          key: "description",
+          name: "description",
           content: `Learn more about ${this.$page.post.title}`,
         },
         {
-          key: 'og:title',
-          name: 'og:title',
+          key: "og:title",
+          name: "og:title",
           content: `${this.$page.post.title} | Intercon Regenerative Center`,
         },
         {
-          key: 'og:site_name',
-          name: 'og:site_name',
-          content: 'Intercon Regenerative Center',
+          key: "og:site_name",
+          name: "og:site_name",
+          content: "Intercon Regenerative Center",
         },
         {
-          key: 'og:image',
-          name: 'og:image',
+          key: "og:image",
+          name: "og:image",
           content: `${this.$page.post.header_image.src}`,
         },
         {
-          name: 'og:description',
-          name: 'og:description',
+          name: "og:description",
+          name: "og:description",
           content: `A comprehensive read about ${this.$page.post.title}`,
         },
       ],
-    }
+    };
   },
   methods: {
-    log: function () {
-      console.log(this.$page.post)
+    log: function() {
+      console.log(this.$page.post);
     },
   },
   components: {
     JournalContent,
+    SocialShareButtons,
   },
   metaInfo() {
     return {
       title: this.$page.post.title,
-    }
+    };
   },
-}
+};
 </script>
 
 <style scoped>
