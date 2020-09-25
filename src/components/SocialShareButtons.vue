8b716e0e8cf109cb3bb1287a6c976519dea17f13
Gabriel Tan <gabtan99@gmail.com>
2020-09-25 16:12:37 +0800

    feat: sharing to facebook button

--- src/components/SocialShareButtons.vue
+++ src/components/SocialShareButtons.vue
@@ -2,16 +2,31 @@
   <div>
     <div
       class="fb-share-button"
-      data-href="https://developers.facebook.com/docs/plugins/"
+      data-href="https://www.your-domain.com/your-page.html"
       data-layout="button_count"
-      data-size="small"
     >
-      <a
-        target="_blank"
-        href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
-        class="fb-xfbml-parse-ignore"
+      <a target="_blank" :href="this.url" class="fb-xfbml-parse-ignore"
         >Share</a
       >
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: "",
+    };
+  },
+
+  mounted() {
+    if (process.isClient) {
+      this.url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+        window.location.href
+      )}&amp;src=sdkpreparse`;
+      // "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse";
+    }
+  },
+};
+</script>
